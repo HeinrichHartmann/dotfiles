b5836e2628de15c05a18d4782be6f9b89e0de0b5
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-02-04 21:18:35 -0500

    update

---
+++ .shell.d/ls.sh
@@ -0,0 +1,29 @@
+function cd() #@ DESCRIPTION: Change directory, print most recent files
+{             #@ USAGE: cd DIR
+    local dir=$1
+    if test -n "$dir"     ## non-empty string?
+    then
+        pushd "$dir" > /dev/null
+    else
+        pushd "$HOME"
+    fi # 1>&2 2>/dev/null   ## do not show DIRSTACK listing and error
+    printf "Dirstack:\n"
+    dirs -v
+    if test $? -ne 0      ## pushd failed?
+    then
+        builtin cd "$dir" ## let cd provide error message
+    else                  ## print content
+        printf "Contents:\n"
+        # ls
+        ls -lt | tail -n+2 | head -n 5 | while read line; do echo "* $line"; done
+        printf "\n"
+    fi
+}
+
+function lsd {
+    ls -l $@ | grep ^d | cat
+}
+
+function dirs {
+    builtin dirs -v
+}
