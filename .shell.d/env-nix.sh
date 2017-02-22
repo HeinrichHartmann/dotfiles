7a1553731b2f93daee6d2553e77025a2ff586460
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-02-22 14:25:11 +0100

    Fix nix support

--- .shell.d/env-nix.sh
+++ .shell.d/env-nix.sh
@@ -1,6 +1,7 @@
 if [[ -e $HOME/.nix-profile ]]
 then
 
-source /export/home/hhartmann/.nix-profile/etc/profile.d/nix.sh
+    [ -f /export/home/hhartmann/.nix-profile/etc/profile.d/nix.sh ]  && \
+        source /export/home/hhartmann/.nix-profile/etc/profile.d/nix.sh
 
 fi
