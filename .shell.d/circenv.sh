1d3ed32ebf6c3bdda511758928e1ea053457fcb5
Heinrich Hartmann <heinrich.hartmann@circonus.com>
2017-02-22 13:11:35 +0000

    Fix tmux colors

--- .shell.d/circenv.sh
+++ .shell.d/circenv.sh
@@ -6,14 +6,14 @@ case "$HOST_CLASS" in
     DEV*)
         PS1='\e[1;34m[CIRC/DEV/\h]\e[m \u:\w\n\$ '
         PROMPT='[CIRC/DEV/%m] %n:%/'$'\n''$ '
-        tmux set -g status-bg colour221 || true
-        tmux set -g status-fg colour0 || true
+        tmux set -g status-bg colour221 > /dev/null || true
+        tmux set -g status-fg colour0   > /dev/null || true
         ;;
     PROD*)
         PS1='\e[1;31m[CIRC/PROD/\h]\e[m \u:\w\n\$ '
         PROMPT='[CIRC/PROD/%m] %n:%/'$'\n''$ '
-        tmux set -g status-bg colour9 || true
-        tmux set -g status-fg colour0 || true
+        tmux set -g status-bg colour9 > /dev/null || true
+        tmux set -g status-fg colour0 > /dev/null || true
         ;;
     PRIV*)
         ;;
