f16bfdc3b2e42f22a3db2f41914f793a409a75a9
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-09-26 15:19:24 +0200

    .

--- .shell.d/timeline.sh
+++ .shell.d/timeline.sh
@@ -1,17 +1,24 @@
-function tl-add {
-  HEAD="$1"; shift
-  BODY="$*"
-  printf "\n* %s %s\n" $(date +%Y-%m-%d) "$*"  >> ~/notes/timeline.org
-  cd ~/notes
-  git add ~/notes/timeline.org
+TL_FILE=~/notes/timeline.org
+
+function tl-commit {
+  cd "$(dirname $TL_FILE)"
+  git add "$TL_FILE"
   git commit -m "."
   git show HEAD
 }
 
+function tl-add {
+  HEAD="$1"; shift
+  BODY="$*"
+  printf "\n* %s \n%s\n" $(date +%Y-%m-%d) "$HEAD" "$BODY" >> "$TL_FILE"
+  tl-commit
+}
+
 function tl-cat {
-  cat ~/notes/timeline.org
+  cat $TL_FILE
 }
 
 function tl-edit {
-  eval $EDITOR ~/notes/timeline.org
+  emacsclient --create-frame --tty -e '(tl-edit "'"$TL_FILE"'")'
+  tl-commit
 }
