1482fef96ec72af08d103401ed3a84b213d9f615
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2019-04-14 15:14:41 +0200

    .

--- .shell.d/ytdl.sh
+++ .shell.d/ytdl.sh
@@ -1,11 +1,9 @@
-# set this in your /etc/hosts
-YTDL_HOST=ytdl_host
-
 function YTDL {
-  FOLDER="~/Attic/Videos"
+  FOLDER="~/YTDL"
   OPTS="--format '"'best[height<=760]'"' --recode-video mp4 --restrict-filenames -o '%(upload_date)s CHANNEL=%(uploader)s TITLE=%(title)s.%(ext)s'"
   POST_CMD=""
   PRE_CMD=""
+  REMOTE=""
   while [[ "$#" -gt 0 ]]
   do
     echo "> $1"
@@ -31,6 +29,11 @@ function YTDL {
         PRE_CMD="mkdir -p $1; cd $1; $PRE_CMD"
         shift
         ;;
+      -r|--remote)
+        shift
+        REMOTE=$1
+        shift
+        ;;
       *)
         break
         ;;
@@ -41,5 +44,10 @@ function YTDL {
   [[ $URL ]] || { echo "No URL provided"; return }
   OPTS="$OPTS $*"
   CMD="cd $FOLDER; $PRE_CMD youtube-dl $OPTS $URL $POST_CMD"
-  printf '%s\n' "$CMD" | ssh YTDL_HOST -- bash -x
+  if [[ -n "$REMOTE" ]]
+  then
+    printf '%s\n' "$CMD" | ssh "$REMOTE" -- bash -x
+  else
+    printf '%s\n' "$CMD" | bash -x
+  fi
 }
