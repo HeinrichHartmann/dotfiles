2efbb4b1a126d18e5cf9c731cf01fde2138848ce
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2020-06-10 09:14:37 +0000

    Add fzf.sh

---
+++ .shell.d/fzf.sh
@@ -0,0 +1,20 @@
+function fzf-man {
+  bash -c "compgen -ac" | fzf --preview 'man {}'
+}
+
+function fzf-book-shelf {
+  (cd ~/Shelf/Books; open "$(find . | fzf -i -e --preview 'pdftotext -l 3 {} -')" )
+}
+
+function fzf-book-archive {
+  /usr/bin/osascript -e 'mount volume "smb://u16/share"'
+  (cd /Volumes/share/attic/Books; push-and-open "$(fzf -i -e --preview 'pdftotext -l 3 {} -' < index.lst)")
+}
+
+function fzf-hist {
+  <~/.zsh_history awk 'BEGIN{FIELDWIDTHS="2 10 3 *"} { print strftime("%F %T", $2) "\t" $4 }' | fzf --no-sort --tac
+}
+
+function fzf-git-log {
+  git log --oneline | fzf --multi --preview 'git show {+1}'
+}
