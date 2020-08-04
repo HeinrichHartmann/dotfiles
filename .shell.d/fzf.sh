90cfe589aab014ca392056a96cac4bc63cae6c58
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2020-08-04 09:22:04 +0000

    .

--- .shell.d/fzf.sh
+++ .shell.d/fzf.sh
@@ -12,9 +12,14 @@ function fzf-book-archive {
 }
 
 function fzf-hist {
-  <~/.zsh_history awk 'BEGIN{FIELDWIDTHS="2 10 3 *"} { print strftime("%F %T", $2) "\t" $4 }' | fzf --no-sort --tac
+  # print -z 
+  print -z $(<~/.zsh_history awk 'BEGIN{FIELDWIDTHS="2 10 3 *"} { print strftime("%F %T", $2) "\t" $4 }' | fzf --no-sort --tac | sed 's/.*	//g')
 }
 
 function fzf-git-log {
   git log --oneline | fzf --multi --preview 'git show {+1}'
+}
+
+function fzf-ag {
+  ag --nobreak --nonumbers --noheading ${1:-.} | fzf --no-sort
 }
